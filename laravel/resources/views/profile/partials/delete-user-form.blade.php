cd77715ff053467ee86843237c4b8bd4e7520f6b
githunb yuki918 <tada950918@gmail.com>
2023-12-03 22:46:28 +0900

    ログイン機能の追加

--- laravel/resources/views/profile/partials/delete-user-form.blade.php
+++ laravel/resources/views/profile/partials/delete-user-form.blade.php
@@ -4,26 +4,26 @@
             {{ __('Delete Account') }}
         </h2>
 
-        <p class="mt-1 text-sm text-gray-600">
+        {{-- <p class="mt-1 text-sm text-gray-600">
             {{ __('Once your account is deleted, all of its resources and data will be permanently deleted. Before deleting your account, please download any data or information that you wish to retain.') }}
-        </p>
+        </p> --}}
     </header>
 
-    <div class="buttonType01_wrap">
+    {{-- <div class="buttonType01_wrap">
       <x-danger-button
           x-data=""
           x-on:click.prevent="$dispatch('open-modal', 'confirm-user-deletion')"
       >{{ __('Delete Account') }}</x-danger-button>
-    </div>
+    </div> --}}
 
     <x-modal name="confirm-user-deletion" :show="$errors->userDeletion->isNotEmpty()" focusable>
         <form method="post" action="{{ route('profile.destroy') }}" class="p-6">
             @csrf
             @method('delete')
 
-            <h2 class="account-info-delete-title text-lg font-medium text-gray-900">
+            {{-- <h2 class="account-info-delete-title text-lg font-medium text-gray-900">
                 {{ __('Are you sure you want to delete your account?') }}
-            </h2>
+            </h2> --}}
 
             <p class="mt-1 text-sm text-gray-600">
                 {{ __('Once your account is deleted, all of its resources and data will be permanently deleted. Please enter your password to confirm you would like to permanently delete your account.') }}
@@ -44,9 +44,9 @@
             </div>
 
             <div class="buttonType01_wrap">
-                <x-secondary-button x-on:click="$dispatch('close')">
+                {{-- <x-secondary-button x-on:click="$dispatch('close')">
                     {{ __('Cancel') }}
-                </x-secondary-button>
+                </x-secondary-button> --}}
 
                 <x-danger-button class="ms-3">
                     {{ __('Delete Account') }}
