e7bb7aa33d8ed0bfab7bc686f3eeee68c613ece7
githunb yuki918 <tada950918@gmail.com>
2023-12-03 17:42:44 +0900

    ログイン機能の追加

--- laravel/app/Providers/AppServiceProvider.php
+++ laravel/app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Routing\UrlGenerator;
+use Illuminate\Support\Facades\URL; 
 
 class AppServiceProvider extends ServiceProvider
 {
